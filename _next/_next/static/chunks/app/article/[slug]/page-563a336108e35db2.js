9a60a11ae727fc9583ea7a5111136c5ec6761be0
krnk0 <krnk0@users.noreply.github.com>
2025-11-24 06:40:01 +0000

    deploy: ef7012f530866d84cc51327d6760806301d9388d

---
+++ _next/_next/static/chunks/app/article/[slug]/page-563a336108e35db2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[225],{1491:()=>{},5511:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1491,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[562,441,684,358],()=>s(5511)),_N_E=e.O()}]);
